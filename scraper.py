6330dd3a879af508fe42dc4038e5f3b148ec16f8
chaitanyavp <chaitanya1998@gmail.com>
2018-05-21 11:52:12 -0400

    html parsed

--- scraper.py
+++ scraper.py
@@ -1,4 +1,9 @@
 import requests
+from bs4 import BeautifulSoup
 
 page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
-print("ye", page)
+parsed = BeautifulSoup(page.content, 'html.parser')
+soup_str = parsed.prettify()
+
+
+print("ye", list(parsed.children))
