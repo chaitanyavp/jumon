c5a5f2877c8d6966ba68c9b14323206853bbd333
chaitanyavp <chaitanya1998@gmail.com>
2018-05-21 13:04:06 -0400

    Fixed title and formatting

--- scraper.py
+++ scraper.py
@@ -3,11 +3,14 @@ from bs4 import BeautifulSoup
 import sys
 
 
-toadparams = {'search_words': 'abyssmegalo', 'searchmode': 'basic'}
+toadparams = {'search_words': sys.argv[1], 'searchmode': 'basic'}
 page = requests.get("https://www.trollandtoad.com/products/search.php", params=toadparams)
 parsed = BeautifulSoup(page.content, 'html.parser')
-prices = parsed.find_all(class_='price_text')
+prices = parsed.find_all(class_='search_result_text')
 
-for x in prices:
-    print ("ye", x.get_text())
-print("ye", prices[0].get_text())
+for x in prices[:20]:
+	text = x.get_text()
+	if text[-13:] == "SELL US YOURS":
+		print("ye", text[:-13])
+	else:
+		print("ne", text)
