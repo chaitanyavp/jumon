106b0537040fef3b4e2c9edbba58493aea801409
chaitanyavp <chaitanya1998@gmail.com>
2018-05-21 12:16:02 -0400

    ebay url

--- scraper.py
+++ scraper.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+page = requests.get("https://www.ebay.ca/sch/i.html?_from=R40&_trksid=p2047675.m570.l1313.TR0.TRC0.H0.Xallure+of+darkness.TRS0&_nkw=allure+of+darkness&_sacat=0")
 parsed = BeautifulSoup(page.content, 'html.parser')
 soup_str = parsed.prettify()
 
-
-print("ye", list(parsed.children))
+print("ye", list(parsed.children)[0])
