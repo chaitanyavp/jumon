b6287d31574e558defee23d1ce8fa9ac9fe25844
chaitanyavp <chaitanya1998@gmail.com>
2018-08-19 21:05:52 -0400

    tabs to spaces

--- scraper.py
+++ scraper.py
@@ -2,82 +2,85 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 
+
 def get_price(line):
-	price = ""
-	num = False
-	for c in line:
-		if c == '$':
-			num = True
-		elif c == ' ' and num:
-			num = False
-			break
-		elif c == ' ':
-			num = False
-		elif num:
-			price += c
-	if price == '':
-		return 9999999
-	else:
-		return float(price)
+    price = ""
+    num = False
+    for c in line:
+        if c == '$':
+            num = True
+        elif c == ' ' and num:
+            num = False
+            break
+        elif c == ' ':
+            num = False
+        elif num:
+            price += c
+    if price == '':
+        return 9999999
+    else:
+        return float(price)
 
 
 def price_sort(lines):
-	if len(lines) > 1:
-		half = int(len(lines)/2)
-		first_half = price_sort(lines[:half])
-		second_half = price_sort(lines[half:])
-		i, j = 0, 0
-		final_lines = []
+    if len(lines) > 1:
+        half = int(len(lines)/2)
+        first_half = price_sort(lines[:half])
+        second_half = price_sort(lines[half:])
+        i, j = 0, 0
+        final_lines = []
 
-		while i < len(first_half) and j < len(second_half):
-			if get_price(first_half[i]) < get_price(second_half[j]):
-				final_lines.append(first_half[i])
-				i += 1
-			else:
-				final_lines.append(second_half[j])
-				j += 1
+        while i < len(first_half) and j < len(second_half):
+            if get_price(first_half[i]) < get_price(second_half[j]):
+                final_lines.append(first_half[i])
+                i += 1
+            else:
+                final_lines.append(second_half[j])
+                j += 1
 
-		if i < len(first_half):
-			final_lines += first_half[i:]
-		elif j < len(second_half):
-			final_lines += second_half[j:]
+        if i < len(first_half):
+            final_lines += first_half[i:]
+        elif j < len(second_half):
+            final_lines += second_half[j:]
 
-		return final_lines
+        return final_lines
 
-	elif len(lines) == 1:
-		return [lines[0]]
-	else:
-		return []
+    elif len(lines) == 1:
+        return [lines[0]]
+    else:
+        return []
+
 
 def scrape(word):
-	toadparams = {'search_words': word, 'search_category': '', 'search_order': 'price_asc'}
-	page = requests.get("https://www.trollandtoad.com/products/search.php", params=toadparams)
-	parsed = BeautifulSoup(page.content, 'html.parser')
-	prices = parsed.find_all(class_='search_result_text')
+    toadparams = {'search_words': word, 'search_category': '', 'search_order': 'price_asc'}
+    page = requests.get("https://www.trollandtoad.com/products/search.php", params=toadparams)
+    parsed = BeautifulSoup(page.content, 'html.parser')
+    prices = parsed.find_all(class_='search_result_text')
 
-	unsorted_lines = []
-	for x in prices:
-		text = x.get_text()
-		if text[-13:] == "SELL US YOURS":
-			unsorted_lines.append(text[:-13])
-		else:
-			unsorted_lines.append(text)
+    unsorted_lines = []
+    for x in prices:
+        text = x.get_text()
+        if text[-13:] == "SELL US YOURS":
+            unsorted_lines.append(text[:-13])
+        else:
+            unsorted_lines.append(text)
 
-	# sorted_lines = price_sort(unsorted_lines)
+    # sorted_lines = price_sort(unsorted_lines)
 
-	return (unsorted_lines[:20], 200)
+    return (unsorted_lines[:20], 200)
+
 
 def scrape_ebay(word):
-	search_terms = "+".join(word.split() + ["yugioh"])
-	page = requests.get("http://www.ebay.com/sch/i.html?_nkw="+search_terms+"&_sacat=0")
-	parsed = BeautifulSoup(page.content, 'html.parser')
-	prices = parsed.find_all(class_="s-item__link")
+    search_terms = "+".join(word.split() + ["yugioh"])
+    page = requests.get("http://www.ebay.com/sch/i.html?_nkw="+search_terms+"&_sacat=0")
+    parsed = BeautifulSoup(page.content, 'html.parser')
+    prices = parsed.find_all(class_="s-item__link")
 
-	for listing in prices:
-		if listing is not None:
-			# print("ye", BeautifulSoup)
-			# parsed_listing = BeautifulSoup(listing, "html.parser")
-			print(listing.get_text(), file=sys.stderr)
-		else:
-			print("ne")
-	return prices
+    for listing in prices:
+        if listing is not None:
+            # print("ye", BeautifulSoup)
+            # parsed_listing = BeautifulSoup(listing, "html.parser")
+            print(listing.get_text(), file=sys.stderr)
+        else:
+            print("ne")
+    return prices
--- server.py
+++ server.py
@@ -7,23 +7,25 @@ import random, json
 
 app = Flask(__name__, template_folder='jumon-app/src')
 
+
 @app.route("/index.html")
 def output():
-	return render_template('index.html', name='Joe')
-	
+    return render_template('index.html', name='Joe')
+
+
 @app.route('/receiver', methods = ['POST'])
 def worker():
-	
-	name = request.args.get('name')
-	print(request.args.get('name'), file=sys.stderr)
-	ebay_scrap = scraper.scrape_ebay(name)
-	data = scraper.scrape(name)[0]
-	str2 = ""
-	
-	for x in data:
-		str2 += x + '\n'
-	
-	return str2
-	
+
+    name = request.args.get('name')
+    print(request.args.get('name'), file=sys.stderr)
+    ebay_scrap = scraper.scrape_ebay(name)
+    data = scraper.scrape(name)[0]
+    str2 = ""
+
+    for x in data:
+        str2 += x + '\n'
+
+    return str2
+
 if __name__ == "__main__":
-	app.run("127.0.0.1", "8080")
+    app.run("127.0.0.1", "8080")
